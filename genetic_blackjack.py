9bd4a6e63906470c9bc7d9d8836db0bfb4c39977
Filip Rudy <filip.rudy5@gmail.com>
2024-06-01 14:03:06 +0200

    Fix players crossover

--- genetic_blackjack.py
+++ genetic_blackjack.py
@@ -7,37 +7,44 @@ from neural_network import NeuralNetwork
 
 env = gym.make("Blackjack-v1")
 
-def generate_population(size: int) -> List[Player]:
-  population = []
-  for _ in range(size):
-    strategy = NeuralNetwork()
-    population.append(Player(strategy))
 
-  return population
+def generate_population(size: int) -> List[Player]:
+    population = []
+    for _ in range(size):
+        strategy = NeuralNetwork()
+        population.append(Player(strategy))
+
+    return population
+
 
 def compute_fitness(player: Player, num_of_games: int):
-  score = 0
-  for _ in range(num_of_games):
-    observation, _ = env.reset()
-    termindated, truncated = False, False
-    while not (termindated and truncated):
-      input_data = torch.tensor(observation).unsqueeze(0)
-      action = player.decide(input_data)
-      observation, reward, termindated, truncated, _ = env.step(action)
-      score += reward
-    return score / reward
-      
-def select_n_top_players(population: List[Player], n:int):
-  sorted_population = sorted(population, key=lambda p: p.get_fitness(), reverse=True)
-  
-  return sorted_population[:n]
+    score = 0
+    for i in range(num_of_games):
+        observation, _ = env.reset()
+        terminated, truncated = False, False
+        while not (terminated or truncated):
+            parsed_observation = (float(observation[0]), float(observation[1]), float(observation[2]))
+            input_data = torch.tensor(parsed_observation).unsqueeze(0)
+            action = player.decide(input_data)
+            observation, reward, terminated, truncated, _ = env.step(action)
+            if reward == 1:
+                score += reward
+    return score / num_of_games
+
+
+def select_n_top_players(population: List[Player], n: int):
+    sorted_population = sorted(population, key=lambda p: p.get_fitness(), reverse=True)
+
+    return sorted_population[:n]
+
 
 def crossover(player1: Player, player2: Player) -> Player:
-  return player1.crossover(player2)
+    return player1.crossover(player2)
+
 
 def mutate(player: Player, mutation_rate: float):
-  return player.mutate(mutation_rate)
-    
-    
+    return player.mutate(mutation_rate)
+
+
 def evolve_population(population: List[Player], top_n: int, mutation_rate: float):
-  pass
+    pass
--- player.py
+++ player.py
@@ -1,53 +1,57 @@
 import random
 from torch import Tensor, argmax, cat, rand, rand_like
-from neural_network import NeuralNetwork
-class Player:
-  def __init__(self, strategy=None):
-      self.strategy = strategy
-      self.__fitness = 0
-  
-  def get_name(self):
-    return self.__name
-  
-  def get_strategy(self):
-    return self.__strategy
-  
-  def get_fitness(self) -> int:
-    return self.__fitness
-  
-  def give_reward(self, reward: int):
-    self.__fitness += reward
-    
-  def get_risk_tolerance(self) -> float:
-    return self.__risk_tolerance
-    
-  def decide(self, input_data: Tensor) -> int:
-    output = self.strategy(input_data)
 
-    return argmax(output).item()
-    
-  def mutate(self, mutation_rate = 0.5):
-    for param in self.strategy.parameters():
-      if rand(1).item() < mutation_rate:
-        param.data += rand_like(param.data) * 0.1
-        
-  def crossover(self, other: 'Player') -> 'Player':
-    child1 = Player(NeuralNetwork())
-    child2 = Player(NeuralNetwork())
-    
-    crossover_point_fc1 = random.randint(2, 14)
-    child1.strategy.fc1.bias.data = cat((self.strategy.fc1.weight.data[:crossover_point_fc1], other.strategy.fc1.weight.data[crossover_point_fc1:]), dim=0)
-    child2.strategy.fc1.bias.data = cat((self.strategy.fc1.weight.data[:crossover_point_fc1], other.strategy.fc1.weight.data[crossover_point_fc1:]), dim=0)
-    
-    child1.strategy.fc1.bias.data = cat((self.strategy.fc1.bias.data[:crossover_point_fc1], other.strategy.fc1.bias.data[crossover_point_fc1:]), dim=0)
-    child1.strategy.fc1.bias.data = cat((self.strategy.fc1.bias.data[:crossover_point_fc1], other.strategy.fc1.bias.data[crossover_point_fc1:]), dim=0)
-    
-    crossover_point_fc2 = random.randint(1, 3)
-    
-    child1.strategy.fc2.bias.data = cat((self.strategy.fc2.weight.data[:crossover_point_fc2], other.strategy.fc2.weight.data[crossover_point_fc2:]), dim=0)
-    child2.strategy.fc2.bias.data = cat((self.strategy.fc2.weight.data[:crossover_point_fc2], other.strategy.fc2.weight.data[crossover_point_fc2:]), dim=0)
-    
-    child1.strategy.fc2.bias.data = cat((self.strategy.fc2.bias.data[:crossover_point_fc2], other.strategy.fc2.bias.data[crossover_point_fc2:]), dim=0)
-    child1.strategy.fc2.bias.data = cat((self.strategy.fc2.bias.data[:crossover_point_fc2], other.strategy.fc2.bias.data[crossover_point_fc2:]), dim=0)
-    
-    return child1, child2
+import neural_network
+from neural_network import NeuralNetwork
+
+
+class Player:
+    def __init__(self, strategy=None):
+        self.strategy = strategy
+        self.__fitness = 0
+
+    def get_name(self):
+        return self.__name
+
+    def get_strategy(self):
+        return self.__strategy
+
+    def get_fitness(self) -> int:
+        return self.__fitness
+
+    def give_reward(self, reward: int):
+        self.__fitness += reward
+
+    def get_risk_tolerance(self) -> float:
+        return self.__risk_tolerance
+
+    def decide(self, input_data: Tensor) -> int:
+        input_data = input_data.view(-1, 3)
+        output = self.strategy(input_data)
+        return argmax(output).item()
+
+    def mutate(self, mutation_rate=0.5):
+        for param in self.strategy.parameters():
+            if rand(1).item() < mutation_rate:
+                param.data += rand_like(param.data) * 0.1
+        return self
+
+    def crossover(self, other: 'Player') -> ('Player', 'Player'):
+        child1 = Player(neural_network.NeuralNetwork())
+        child2 = Player(neural_network.NeuralNetwork())
+
+        for param1, param2, param_self, param_other in zip(child1.strategy.parameters(), child2.strategy.parameters(),
+                                                           self.strategy.parameters(), other.strategy.parameters()):
+            # Flatten parameters to 1D for easy crossover
+            param1_data = param_self.data.view(-1)
+            param2_data = param_other.data.view(-1)
+
+            crossover_point = random.randint(0, param1_data.size(0))
+
+            # Perform crossover
+            param1.data = cat((param1_data[:crossover_point], param2_data[crossover_point:]), 0).view(
+                param_self.data.shape)
+            param2.data = cat((param2_data[:crossover_point], param1_data[crossover_point:]), 0).view(
+                param_other.data.shape)
+
+        return child1, child2
